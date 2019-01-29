36437ef6c75ca69a14dfb92aa709d159bd247698
BlackRaver <blackravern01@gmail.com>
2019-01-29 15:30:40 +0100

    2 wersja

--- app/src/main/java/com/example/blackravern/prba/FullscreenActivity.java
+++ app/src/main/java/com/example/blackravern/prba/FullscreenActivity.java
@@ -90,8 +90,7 @@ public class FullscreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fullscreen);
 
         mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
+
 
 
         // Set up the user interaction to manually show or hide the system UI.
@@ -105,7 +104,7 @@ public class FullscreenActivity extends AppCompatActivity {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+
     }
 
     @Override
